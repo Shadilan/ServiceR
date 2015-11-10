8275a81af7188b00ea39ae0e56c1f60e499deb76
Shadilan <shadilan@gmail.com>
2015-11-10 09:39:44 +0300

    correct creation ambush. Create not only in Trap but in allObjects table

--- src/main/java/main/PlayerObj.java
+++ src/main/java/main/PlayerObj.java
@@ -283,11 +283,17 @@ public class PlayerObj implements GameObject {
 		try {
 			Connection con = DBUtils.ConnectDB();
 			String GUID_ROUTE = UUID.randomUUID().toString();
+			//TODO: Need to make object Ambush and do creation through it
 			stmt = con.prepareStatement("insert into traps (GUID, OWNER, LAT, LNG) VALUES (?,?,?,?)");
 			stmt.setString(1, GUID);
 			stmt.setString(2, Owner);
 			stmt.setInt(3, Lat);
 			stmt.setInt(4, Lng);
+			stmt.execute();
+			stmt = con.prepareStatement("insert into aobject (GUID, LAT, LNG,ObjectType) VALUES (?,?,?,'AMBUSH')");
+			stmt.setString(1, GUID);
+			stmt.setInt(2, Lat);
+			stmt.setInt(3, Lng);
 			stmt.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
