5d64ed09d108f2cdcb7e49209028db15ad56be95
Zlodiak <evil.aaz@gmail.com>
2015-11-21 17:59:21 +0300

    Merge branch 'master' of ssh://service-merchantarg.rhcloud.com/~/git/service
    # Conflicts:
    #	src/main/java/main/SpiritProto.java

--- src/main/java/main/SpiritProto.java
+++ src/main/java/main/SpiritProto.java
@@ -257,7 +257,7 @@ public class SpiritProto {
                         } else {result=check;};
                         break;
                     default:
-                        result = MyUtils.getJSONError("ActtionNotFound", "Действие не определено");
+                        result = MyUtils.getJSONError("ActionNotFound", "Действие не определено");
                 }
             } else
             {
